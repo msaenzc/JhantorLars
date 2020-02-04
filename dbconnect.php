678c6f52dc3ef11907c9ca21b7be40863da3c144
Mauricio Saenz <msaenzc@gmail.com>
2020-02-03 23:24:35 -0300

    My first commit

---
+++ dbconnect.php
@@ -0,0 +1,4 @@
+<?php
+$dbh=mysql_connect ("localhost", "teletrab_msaenz", "carla0110") or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ("teletrab_teletotal"); 
+?>
